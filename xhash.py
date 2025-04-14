caa05d7467d08fc1b980e61893323845149f9d20
MetaurOs <wolf.metaur0s@gmail.com>
2025-04-14 04:03:14 -0300

    Add files via upload

--- xhash.py
+++ xhash.py
@@ -34,10 +34,20 @@ def xhash256 (v):
 assert xhash16(b'') == 0
 assert xhash64(b'') == 0
 
+assert 0x3DE7 == xhash16(b'abcdefghijklmnopqrstuvwxyz')
+assert 0xE31C == xhash16(b'abcdefghijklmnopqrstuvwxyz0123456789')
+assert 0x9D22 == xhash16(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+assert 0x9DB6 == xhash16(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+
 assert 0x5FED == xhash16(b'ABACATE')
 assert 0x2367 == xhash16(b'CENOURA')
 assert 0xF097 == xhash16(b'ALFACE')
 assert 0xB06A == xhash16(b'BETERRABA')
+
+assert 0x3E85D1C3472194DF == xhash64(b'abcdefghijklmnopqrstuvwxyz')
+assert 0xB0EC7AA97BA92D41 == xhash64(b'abcdefghijklmnopqrstuvwxyz0123456789')
+assert 0xDDA0F4C5129778F5 == xhash64(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+assert 0x709A1D95A190F463 == xhash64(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 
 assert 0xFA24DA0E912BDDDF == xhash64(b'ABACATE')
 assert 0x1FDB8771F28B27A4 == xhash64(b'CENOURA')
